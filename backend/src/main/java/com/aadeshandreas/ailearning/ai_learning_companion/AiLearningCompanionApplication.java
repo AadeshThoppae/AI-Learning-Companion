77e08312e1c50445e3e022d0a06ebc1e68015d90
Aadeshh <athoppae@gmail.com>
2025-09-08 13:09:20 -0700

    basic project skeleton - react front end, spring boot setup for backend

---
+++ backend/src/main/java/com/aadeshandreas/ailearning/ai_learning_companion/AiLearningCompanionApplication.java
@@ -0,0 +1,13 @@
+package com.aadeshandreas.ailearning.ai_learning_companion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiLearningCompanionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiLearningCompanionApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/aadeshandreas/ailearning/ai_learning_companion/AiLearningCompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aadeshandreas.ailearning.ai_learning_companion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiLearningCompanionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
