e7ce0c244a3ed7eb9c4ce2201d2fc301620820df
AadeshThoppae <akthoppae@dons.usfca.edu>
2025-09-29 11:49:20 -0700

    Update frontend/src/app/page.tsx
    Co-authored-by: Andreas Jack Christiansen <42472984+Dressi123@users.noreply.github.com>

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
     }
 
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="font-sans items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <div className={"container mx-auto px-4 py-8"}>
           <div className={"text-center mb-8"}>
               <div className={"flex items-center justify gap-3 mb-4"}>
