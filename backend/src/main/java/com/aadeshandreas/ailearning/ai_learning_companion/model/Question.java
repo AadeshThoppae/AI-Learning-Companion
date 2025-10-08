c731224370166035baed674a34780ab61a47484b
Aadeshh <athoppae@gmail.com>
2025-10-07 17:58:07 -0700

    added basic backend structure - needs debugging and refinement

---
+++ backend/src/main/java/com/aadeshandreas/ailearning/ai_learning_companion/model/Question.java
@@ -0,0 +1,8 @@
+package com.aadeshandreas.ailearning.ai_learning_companion.model;
+
+public class Question {
+    private String question;
+    private String options;
+    private String answer;
+
+}
---
+++ backend/src/main/java/com/aadeshandreas/ailearning/ai_learning_companion/model/Quiz.java
@@ -0,0 +1,9 @@
+package com.aadeshandreas.ailearning.ai_learning_companion.model;
+
+
+import java.util.List;
+
+public class Quiz {
+    private List<Question> quiz;
+
+}
